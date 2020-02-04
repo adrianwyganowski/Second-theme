9c53b7f245e81f0bd80bc1f4d2cb16b64061802a
adrianwyganowski <52456597+adrianwyganowski@users.noreply.github.com>
2020-02-04 16:27:43 +0100

    added collapsible mini cart and changed js responsible of collapsing menu

---
+++ app/design/frontend/Magento/theme-frontend-second/Magento_Theme/web/js/displayMenu.js
@@ -0,0 +1,21 @@
+define(() => (config, element) => {
+    'use strict'
+    element.addEventListener("click", displayMenuElement);
+    
+    function displayMenuElement(){
+        const htmlTarget = document.getElementsByClassName(config.target)[0];
+        const htmlCheck = document.getElementsByClassName("menuDisplayed")[0];
+
+        if(!htmlCheck){
+            htmlTarget.classList.toggle("menuDisplayed");
+        }
+        else if(htmlTarget === htmlCheck){
+            htmlTarget.classList.toggle("menuDisplayed");
+        }
+        else if((htmlTarget !== htmlCheck)){
+            htmlCheck.classList.toggle("menuDisplayed");
+            htmlTarget.classList.toggle("menuDisplayed");
+        }
+    }
+    
+  });
