04cd0a12dd6b9e98d8b9ad23fd68cd3260a1ea6a
adrianwyganowski <52456597+adrianwyganowski@users.noreply.github.com>
2020-02-11 22:56:48 +0100

    changed filters in catalog page and started working on product page

--- app/design/frontend/Magento/theme-frontend-second/Magento_LayeredNavigation/web/js/showFilters.js
+++ app/design/frontend/Magento/theme-frontend-second/Magento_LayeredNavigation/web/js/showFilters.js
@@ -1,20 +1,9 @@
-define(() => (config) => {
-    'use strict'
-    
-    const arrElements = config.filters;
-    console.log(arrElements);
+define(() => (config, element) => {
+    "use strict";
 
-    for (let i = 0; i < arrElements.length; i++){
-        const element = document.getElementsByClassName(arrElements[i])[0];
-        console.log(element);
-        if (element !== undefined){
-            element.addEventListener("click", displayFilter);
-        }
-        
+    element.addEventListener("click", displayFilter);
+
+    function displayFilter() {
+        this.classList.toggle("filters-displayed");
     }
-    
-    function displayFilter(){
-        this.classList.toggle("filter-displayed");
-    }
-    
-  });
+});
